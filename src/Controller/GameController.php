80ce2047dbe59882ed1989df5229af0ee2a28413
AshBll <61969618+AshBll@users.noreply.github.com>
2020-11-04 14:17:15 +0100

    Update GameController.php

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -68,7 +68,6 @@ class GameController extends AbstractController
             if (false === $this->troopManager->insert($troop)) {
                 header("HTTP/1.0 404 Not Found");
                 echo '404 - Page not found';
-                header('Location: /game/play');
             }
         }
         header('Location: /game/play');
